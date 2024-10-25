38a96cb0e8d418d803f5b1d047c37ee60f8a8305
parkashay <prakaas.poudell@gmail.com>
2024-10-25 16:15:04 +0545

    update: install as an app

--- app/root.tsx
+++ app/root.tsx
@@ -52,6 +52,7 @@ export function links() {
       rel: 'stylesheet',
       href: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap',
     },
+    { rel: 'manifest', href: '/manifest.json' },
   ];
 }
 
